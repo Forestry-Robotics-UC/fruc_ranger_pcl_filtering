1f4e14f4debdd4875c0f469a22062504c8611419
Duda Andrada <duda.andrada@live.com>
2022-05-12 11:43:40 +0100

    'working code for box crop boxes'

--- scripts/pcl_dynamic_crop.py
+++ scripts/pcl_dynamic_crop.py
@@ -19,141 +19,176 @@ from math import asin, acos, atan
 
 
 class arm_dynamic_crop():
-	def __init__(self):
-	    self.tf_buffer = tf2_ros.Buffer()
-	    self.listener = tf2_ros.TransformListener(self.tf_buffer)
-	    self.pcl_sub = rospy.Subscriber("front_lslidar_point_cloud", pc2.PointCloud2, self.pcl_callback, queue_size=1)
-	    self.window_size = [1.45, 1.2, 1.34]
-	    self.input_frame = rospy.get_param('input_frame', 'bobcat_base')
-	    self.output_frame = rospy.get_param('output_frame', "arms_link")
-	    self.crop_node = '/end_effector_crop_box'
-	    self.client = dynamic_reconfigure.client.Client(self.crop_node)
-	    self.params = {'min_x' : -1.0,
-	    			   'max_x' :  1.0,
-	    			   'min_y' : -1.0,
-	    			   'max_y' :  1.0,
-	    			   'min_z' : -1.0,
-	    			   'max_z' :  1.0,
-	    			   'output_frame' : self.input_frame,
-	    			   'input_frame'  : self.output_frame,
-	    			   'negative' : 0,
-	    			   'keep_organized' : 1
+  def __init__(self):
+      self.tf_buffer = tf2_ros.Buffer()
+      self.listener = tf2_ros.TransformListener(self.tf_buffer)
+      self._transform_broadcaster = tf2_ros.TransformBroadcaster()
+      self.arms_link_tf = TransformStamped()
 
-	    }
-	    self._transform_broadcaster = tf2_ros.TransformBroadcaster()
-
-
-	def pcl_callback(self, ros_msg):
-
-		self.params = self.broadcast_tf2()
-
-		# print(self.params)
-		self.client.update_configuration(self.params)
+      self.pcl_sub = rospy.Subscriber("front_lslidar_point_cloud", pc2.PointCloud2, self.pcl_callback, queue_size=1)
+      self.output_frame = rospy.get_param('output_frame', 'bobcat_base')
+      self.input_frame = rospy.get_param('input_frame', "arms_link")
+      self.tool = rospy.get_param('~tool_crop')
+      print(self.tool)
 
 
 
-	def change_frame(self, data, new_frame):
-
-	    return  self.tf_buffer.transform(data, new_frame, rospy.Duration(2.0))
-
-	def publish_tf(self, x, y, z, i, trans, frame_id = 'arms_link'):
-	    ## Publish tf on Rviz
-
-	    transform = TransformStamped()
-	    transform.header.stamp = rospy.Time.now() # Works at the moment
-	    transform.child_frame_id = "pos_"  + str(i)
-	    transform.header.frame_id = frame_id
-	    transform.transform.translation.x = x
-	    transform.transform.translation.y = y
-	    transform.transform.translation.z = z
-	    # transform.transform.rotation.x = 0
-	    # transform.transform.rotation.y = 0
-	    # transform.transform.rotation.z = 0
-	    # transform.transform.rotation.w  = 1
-	    transform.transform.rotation.x = trans.transform.rotation.x
-	    transform.transform.rotation.y = trans.transform.rotation.y
-	    transform.transform.rotation.z = trans.transform.rotation.z
-	    transform.transform.rotation.w  = trans.transform.rotation.w
-	    self._transform_broadcaster.sendTransform(transform)
-	    # print(transform)
-	    return transform
+      self.params = {'min_x' : -1.0,
+               'max_x' :  1.0,
+               'min_y' : -1.0,
+               'max_y' :  1.0,
+               'min_z' : -1.0,
+               'max_z' :  1.0,
+               'output_frame' : self.output_frame,
+               'input_frame'  : self.input_frame,
+               'negative' : 0,
+               'keep_organized' : 1
+      }
 
 
-	def broadcast_tf2(self):
-
-			br = tf2_ros.TransformBroadcaster()
-			t = TransformStamped()
-			tf_buffer = tf2_ros.Buffer()
-			listener = tf2_ros.TransformListener(tf_buffer)
-			t.header.stamp = rospy.Time.now()
-			t.header.frame_id = "bobcat_base"
-			t.child_frame_id = "arms_link"
-			t.transform.translation.x = -1.174615
-			t.transform.translation.y = 0.0
-			t.transform.translation.z = 1.413525
-			try:
-				end_eff = tf_buffer.lookup_transform(self.output_frame, 'end_effector_link', rospy.Time(), rospy.Duration(3.0))
-				arms = tf_buffer.lookup_transform(self.input_frame, 'end_effector_link', rospy.Time(), rospy.Duration(3.0))
-
-				if arms.transform.translation.z > 2.73:
-				    arms.transform.translation.z = 2.73
-				value = (arms.transform.translation.z-1.413525)/2.5
-				q = tf_conversions.transformations.quaternion_from_euler(0,
-				                                                         -atan(value)-0.413525,
-				                                                         0)
-				t.header.stamp = rospy.Time.now()
-				t.header.frame_id = "bobcat_base"
-				t.child_frame_id = "arms_link"
-				t.transform.translation.x = -1.174615
-				t.transform.translation.y = 0.0
-				t.transform.translation.z = 1.413525
-				t.transform.rotation.x = q[0]
-				t.transform.rotation.y = q[1]
-				t.transform.rotation.z = q[2]
-				t.transform.rotation.w = q[3]
-				self._transform_broadcaster.sendTransform(t)
+      if (self.tool == 'camera_tool'):
+        print('at camera tool init')
+        self.cam_tool_crop_node = '/camera_tool_crop_box'
+        self.cam_tool_client = dynamic_reconfigure.client.Client(self.cam_tool_crop_node)
+        self.cam_params = self.params
+      else: 
+        self.end_eff_crop_node = '/end_effector_crop_box'
+        self.end_eff_client = dynamic_reconfigure.client.Client(self.end_eff_crop_node)
+        self.end_eff_params = self.params
 
 
 
-				print(arms.transform.translation.x/1.3,acos(arms.transform.translation.x/1.3))
-				min_x = arms.transform.translation.x
-				max_x = arms.transform.translation.x*1.2 + 1.2
-
-				max_values = self.publish_tf(acos(arms.transform.translation.x/1.3)+ 2.4, 
-																		 t.transform.translation.y + 0.85,
-																		 -acos((arms.transform.translation.z/2.73)) + 1.3,
-																		 'max_values', t)
-				min_values = self.publish_tf(acos(arms.transform.translation.x/1.3) + 0.7, 
-																		 t.transform.translation.y - 0.85,
-																		 -acos((arms.transform.translation.z/2.73)),
-																		 'min_values', t)
-
-				max_frame = PoseStamped()
-				max_frame.header = t.header
-				max_frame.pose.position.x = max_values.transform.translation.x
-				max_frame.pose.position.y =  max_values.transform.translation.y
-				max_frame.pose.position.z =  max_values.transform.translation.z
-
-				min_frame = PoseStamped()
-				min_frame.header = t.header
-				min_frame.pose.position.x = min_values.transform.translation.x
-				min_frame.pose.position.y =  min_values.transform.translation.y
-				min_frame.pose.position.z =  min_values.transform.translation.z
 
 
-				self.params['min_x'] = min_frame.pose.position.x 
-				self.params['max_x'] = max_frame.pose.position.x 
-				self.params['min_y'] = min_frame.pose.position.y 
-				self.params['max_y'] = max_frame.pose.position.y
-				self.params['min_z'] = min_frame.pose.position.z 
-				self.params['max_z'] = max_frame.pose.position.z
+
+  def pcl_callback(self, ros_msg):
+
+    self.broadcast_tf2()
+    if (self.tool == 'camera_tool'):
+      print(self.cam_params)
+      self.cam_tool_client.update_configuration(self.cam_params)
+    else:
+      print(self.end_eff_params)
+
+      self.end_eff_client.update_configuration(self.end_eff_params)
 
 
-			except (tf2_ros.LookupException, 
-			        tf2_ros.ConnectivityException, 
-			        tf2_ros.ExtrapolationException) as e:
-				print (e)
-			return self.params
+  def change_frame(self, data, new_frame):
+
+      return  self.tf_buffer.transform(data, new_frame, rospy.Duration(2.0))
+
+  def publish_tf(self, x, y, z, i, frame_id = 'arms_link'):
+      ## Publish tf on Rviz
+
+      transform = TransformStamped()
+      transform.header.stamp = rospy.Time.now() # Works at the moment
+      transform.child_frame_id = "pos_"  + str(i)
+      transform.header.frame_id = frame_id
+      transform.transform.translation.x = x
+      transform.transform.translation.y = y
+      transform.transform.translation.z = z
+      transform.transform.rotation.x = self.arms_link_tf.transform.rotation.x
+      transform.transform.rotation.y = self.arms_link_tf.transform.rotation.y
+      transform.transform.rotation.z = self.arms_link_tf.transform.rotation.z
+      transform.transform.rotation.w  = self.arms_link_tf.transform.rotation.w
+      self._transform_broadcaster.sendTransform(transform)
+      return transform
+
+
+  def broadcast_tf2(self):
+
+      try:
+
+        end_eff = self.tf_buffer.lookup_transform(self.output_frame, 'end_effector_link', rospy.Time(), rospy.Duration(3.0))
+        camera = self.tf_buffer.lookup_transform(self.output_frame, 'camera_tool_link', rospy.Time(), rospy.Duration(3.0))
+
+
+        if end_eff.transform.translation.z > 2.73:
+            end_eff.transform.translation.z = 2.73
+        value = (end_eff.transform.translation.z-1.413525)/2.5
+        q = tf_conversions.transformations.quaternion_from_euler(0,
+                                                                 -atan(value)-0.463525,
+                                                                 0)
+        self.arms_link_tf.header.stamp = rospy.Time.now()
+        self.arms_link_tf.header.frame_id = self.output_frame
+        self.arms_link_tf.child_frame_id = self.input_frame
+        self.arms_link_tf.transform.translation.x = -1.174615
+        self.arms_link_tf.transform.translation.y = 0.0
+        self.arms_link_tf.transform.translation.z = 1.413525
+        self.arms_link_tf.transform.rotation.x = q[0]
+        self.arms_link_tf.transform.rotation.y = q[1]
+        self.arms_link_tf.transform.rotation.z = q[2]
+        self.arms_link_tf.transform.rotation.w = q[3]
+        self._transform_broadcaster.sendTransform(self.arms_link_tf)
+
+        if (self.tool == 'camera_tool'):
+          # CAMERA TOOL CROPBOX
+          frame = self.output_frame
+          cam_max_values = self.publish_tf(camera.transform.translation.x + 0.8, 
+                                       camera.transform.translation.y + 0.9,
+                                       camera.transform.translation.z + 0.2,
+                                       'cam_max_values', frame)
+          cam_min_values = self.publish_tf(end_eff.transform.translation.x - 0.1, 
+                                       camera.transform.translation.y - 1.1,
+                                       end_eff.transform.translation.z - 0.4,
+                                       'cam_min_values', frame)
+          cam_max_frame = PoseStamped()
+          cam_max_frame.header = cam_max_values.header
+          print(cam_max_frame.header)
+          cam_max_frame.pose.position.x = cam_max_values.transform.translation.x
+          cam_max_frame.pose.position.y =  cam_max_values.transform.translation.y
+          cam_max_frame.pose.position.z =  cam_max_values.transform.translation.z
+          cam_max_frame = self.change_frame(cam_max_frame, self.input_frame)
+          cam_min_frame = PoseStamped()
+          cam_min_frame.header.stamp = rospy.Time.now()
+          cam_min_frame.header.frame_id = frame
+          cam_min_frame.pose.position.x = cam_min_values.transform.translation.x
+          cam_min_frame.pose.position.y =  cam_min_values.transform.translation.y
+          cam_min_frame.pose.position.z =  cam_min_values.transform.translation.z
+          cam_min_frame = self.change_frame(cam_min_frame, self.input_frame)
+          self.cam_params['min_x'] = cam_min_frame.pose.position.x 
+          self.cam_params['max_x'] = cam_max_frame.pose.position.x 
+          self.cam_params['min_y'] = cam_min_frame.pose.position.y 
+          self.cam_params['max_y'] = cam_max_frame.pose.position.y 
+          self.cam_params['min_z'] = cam_min_frame.pose.position.z
+          self.cam_params['max_z'] = cam_max_frame.pose.position.z
+        else:
+          # END EFFECTOR CROPBOX
+          print('here')
+          end_eff_max_values = self.publish_tf(acos(end_eff.transform.translation.x/1.3)+ 2.2, 
+                                       self.arms_link_tf.transform.translation.y + 1.2,
+                                       -acos(((end_eff.transform.translation.z-1.41)/2.73)) + 1.3,
+                                       'max_values')
+          end_eff_min_values = self.publish_tf(acos(end_eff.transform.translation.x/1.3), 
+                                       self.arms_link_tf.transform.translation.y - 1.2,
+                                       -acos((end_eff.transform.translation.z/2.73)),
+                                       'min_values')
+
+          
+
+          end_eff_max_frame = PoseStamped()
+          end_eff_max_frame.header = self.arms_link_tf.header
+          end_eff_max_frame.pose.position.x = end_eff_max_values.transform.translation.x
+          end_eff_max_frame.pose.position.y =  end_eff_max_values.transform.translation.y
+          end_eff_max_frame.pose.position.z =  end_eff_max_values.transform.translation.z
+
+          end_eff_min_frame = PoseStamped()
+          end_eff_min_frame.header = self.arms_link_tf.header
+          end_eff_min_frame.pose.position.x = end_eff_min_values.transform.translation.x
+          end_eff_min_frame.pose.position.y =  end_eff_min_values.transform.translation.y
+          end_eff_min_frame.pose.position.z =  end_eff_min_values.transform.translation.z
+
+          self.end_eff_params['min_x'] = end_eff_min_frame.pose.position.x 
+          self.end_eff_params['max_x'] = end_eff_max_frame.pose.position.x 
+          self.end_eff_params['min_y'] = end_eff_min_frame.pose.position.y 
+          self.end_eff_params['max_y'] = end_eff_max_frame.pose.position.y
+          self.end_eff_params['min_z'] = end_eff_min_frame.pose.position.z 
+          self.end_eff_params['max_z'] = end_eff_max_frame.pose.position.z
+
+      except (tf2_ros.LookupException, 
+              tf2_ros.ConnectivityException, 
+              tf2_ros.ExtrapolationException) as e:
+        print (e)
 
 def main():
   rospy.init_node('cluster_pointcloud', anonymous=True)
